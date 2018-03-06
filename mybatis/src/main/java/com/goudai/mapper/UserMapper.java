a0bbac4b831ae1d597b29cc1371fb6c60a5d35cb
unknown <l>
2018-03-06 16:16:13 +0800

    second commit

--- mybatis/src/main/java/com/goudai/mapper/UserMapper.java
+++ mybatis/src/main/java/com/goudai/mapper/UserMapper.java
@@ -6,13 +6,13 @@ import java.util.List;
 
 public interface UserMapper {
     //新增用户
-    public int insertUser(UserBean user) throws Exception;
+    int insertUser(UserBean user) throws Exception;
     //修改用户
-    public int updateUser(UserBean user, int id) throws Exception;
+    int updateUser(UserBean user, int id) throws Exception;
     //删除用户
-    public int deleteUser(int id) throws Exception;
+    int deleteUser(int id) throws Exception;
     //根据id查询用户信息
-    public UserBean getUserById(int id) throws Exception;
+    UserBean getUserById(int id) throws Exception;
     //查询所有用户信息
-    public List<UserBean> selectAllUser() throws Exception;
+    List<UserBean> selectAllUser() throws Exception;
 }
